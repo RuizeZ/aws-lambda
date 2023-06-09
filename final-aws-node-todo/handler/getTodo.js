6fb9c2572831b10c57c15995ee1fe9d84f1ea7b0
Ruize Zhang <imruize@126.com>
2023-06-08 17:32:21 -0700

    added getTodo and listTodos

---
+++ final-aws-node-todo/handler/getTodo.js
@@ -0,0 +1,29 @@
+const AWS = require("aws-sdk");
+
+const TODO_TABLE = process.env.TODO_TABLE;
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
+
+module.exports.getTodo = (event, context, callback) => {
+    const params = {
+        TableName: TODO_TABLE,
+        Key: {
+            userId: event.pathParameters.userId
+        }
+    }
+    dynamoDb.get(params, (error, data) => {
+        if (error) {
+            console.error(error);
+            callback(new Error)
+            return
+        }
+        const response = data.Item
+            ? {
+                statusCode: 200,
+                body: JSON.stringify(data.Item)
+            } : {
+                statusCode: 404,
+                body: JSON.stringify({ message: "Todo not found" })
+            }
+        callback(null, response)
+    })
+}
